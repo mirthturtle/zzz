1cbb33817c8c4e1f34d6fb7431133458d82a999f
Christian DeWolf <me@christiandewolf.com>
2024-10-29 16:40:51 -0300

    Play sound on ready

--- zzz.py
+++ zzz.py
@@ -4,6 +4,7 @@ from pygame import mixer
 
 print("Initializing...")
 mixer.init()
+ready_sound   = mixer.Sound("sound/piano.mp3")
 trigger_sound = mixer.Sound("sound/sonar.mp3")
 left_sound    = mixer.Sound("sound/pop.mp3")
 right_sound   = mixer.Sound("sound/ding.mp3")
@@ -11,6 +12,8 @@ right_sound   = mixer.Sound("sound/ding.mp3")
 mouse = InputDevice('/dev/input/event0') # or event1, etc. if other devices are present
 
 print("Ready! Listening for input...")
+ready_sound.play()
+
 for event in mouse.read_loop():
     if event.type == ecodes.EV_KEY:
         if event.code == ecodes.BTN_LEFT and event.value == 1:
